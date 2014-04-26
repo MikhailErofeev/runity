143b6edde2965369fcacda618e972bbaf2582169
m-erofeev <m-erofeev@yandex-team.ru>
2014-04-26 23:41:05 +0400

    pathparam

--- src/main/java/com/github/mikhailerofeev/runity/domain/controllers/SearchController.java
+++ src/main/java/com/github/mikhailerofeev/runity/domain/controllers/SearchController.java
@@ -3,6 +3,7 @@ package com.github.mikhailerofeev.runity.domain.controllers;
 import com.github.mikhailerofeev.runity.domain.entities.Employee;
 import com.github.mikhailerofeev.runity.domain.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,14 +14,14 @@ import java.util.List;
  * @since 26.04.14
  */
 @RestController
-@RequestMapping(value = "/rest/v1/search/")
+@RequestMapping(value = "/rest/v1/search")
 public class SearchController {
 
     @Autowired
     EmployeeRepository employeeRepository;
 
-    @RequestMapping(value = "/")
-    public List<Employee> result() {
+    @RequestMapping(value = "/{query}")
+    public List<Employee> result(@PathVariable("query") String query) {
         return employeeRepository.findAll();
     }
 }
