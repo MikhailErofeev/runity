d91e78b62c0789b74fed20c3a33ff352b0d95cf5
m-erofeev <m-erofeev@yandex-team.ru>
2014-04-27 10:56:43 +0400

    encoding

--- src/main/java/com/github/mikhailerofeev/runity/server/MvcConfig.java
+++ src/main/java/com/github/mikhailerofeev/runity/server/MvcConfig.java
@@ -1,11 +1,17 @@
 package com.github.mikhailerofeev.runity.server;
 
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author m-erofeev
@@ -13,6 +19,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  */
 @Configuration
 public class MvcConfig extends WebMvcConfigurerAdapter {
+
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/#!/login").setViewName("login");
@@ -23,5 +30,18 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
         CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
         multipartResolver.setMaxUploadSize(-1);
         return multipartResolver;
+    }
+
+    @Bean
+    @Order(value = 1)
+    public FilterRegistrationBean authorizationFilter() {
+        FilterRegistrationBean filterRegBean = new FilterRegistrationBean();
+        final CharacterEncodingFilter filter = new CharacterEncodingFilter();
+        filter.setEncoding("UTF-8");
+        filterRegBean.setFilter(filter);
+        List<String> urlPatterns = new ArrayList<String>();
+        urlPatterns.add("/*");
+        filterRegBean.setUrlPatterns(urlPatterns);
+        return filterRegBean;
     }
 }
